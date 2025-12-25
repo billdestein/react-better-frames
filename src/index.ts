2f3125d97ef19e55a782c44ed7a415f599d66c15
Bill DeStein <bill@Bills-MacBook-Pro.local>
2025-12-25 15:58:33 -0500

    More work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-export { Button } from './Button'
+// export type Button from './Button'
 export { Canvas } from './Canvas'
 export { Frame } from './Frame'
 export { FrameButtons } from './FrameButtons'
 export { FrameHeader } from './FrameHeader'
 export { FrameTitle } from './FrameTitle'
-export { Geometry } from './Geometry'
+// export { Geometry } from './Geometry'
+
+export type Geometry = {
+  height: number
+  width: number
+  x: number
+  y: number
+  z: number
+}
+
+export type Button = {
+  icon: React.JSX.Element
+  onClick: () => void
+  tip: string
+}
